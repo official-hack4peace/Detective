664bd66e9f6ab475e9a848af29be91071320d1be
hack4peace <90603785+official-hack4peace@users.noreply.github.com>
2022-06-20 22:31:55 +0530

    Update detective.sh

--- detective.sh
+++ detective.sh
@@ -2,19 +2,47 @@ import socket
 import requests
 import phonenumbers
 from phonenumbers import carrier,geocoder,timezone
-print ("⠀⠀        ⠀⠀⠀⣀⡀")⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-print ("⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣾⣿⣿⣷⣶⣤.")⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-print ("⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦")⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-print ("⠀⠀⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧")⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-print ("⠀⠀⠀⠀⠀⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣄")⠀⠀⠀⠀⠀⠀⠀⠀⠀
-print ("⠀⠀⠀⠀⠾⠿⠿⠟⠛⠛⠛⠛⠛⣛⣛⣛⣛⣛⡛⠛⠛⠛.")⠀⠀⠀⠀⠀⠀⠀
-print ("⠀⠀⠀⠀⠀⠀⠰⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⢀⣤⣤⡀")⠀⠀
-print ("⠀⠀⠀⠀⢀⣴⡄⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⢀⣾⠋⡈⢿⡄")⠀
-print ("⠀⠀⢠⣾⣿⣿⣿⣦⡀⠻⢿⣿⣿⣿⣿⣿⣿⠛⠛⠃⠀⠀⠀⣼⡇⠀⠁⢸⡇")⠀
-print ("⠀⣠⣤⣤⣌⣉⠙⠻⢿⣦⣄⠙⠻⠿⣿⡿⠃⠰⣦⠀⠀⠀⠀⣿⡄⠀⠀⣼⠇")⠀
-print ("⠀⣿⣿⣿⣿⣿⣿⣶⣤⣈⠛⢿⣶⣄⠀⠀⠀⠀⢸⠇⠀⠀⠀⠸⣧⣀⣰⠏")⠀⠀
-print ("⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡈⠛⢷⠀⠀⠀⣾⠀⠀⠀⠀⠀⢸⡿⠁")⠀⠀⠀
-print ("⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣄⠀⠀⢸⣿⣿⣷⣦⠀⠀⢸⡇")⠀⠀⠀⠀
-print ("⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⠀⠘⠿⣿⠿⠋⠀⠀⣸⡇")⠀⠀⠀
-Print ("⠀⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠀⠀⠀⠀⠀⠀⠀⠀⠛⠁")
+print "⠀⠀        ⠀⠀⠀⣀⡀"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+print "⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣾⣿⣿⣷⣶⣤."⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+print "⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦"⠀⠀⠀⠀⠀⠀⠀⠀⠀
+print "⠀⠀⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+print "⠀⠀⠀⠀⠀⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣄"⠀⠀⠀⠀⠀⠀⠀⠀
+print "⠀⠀⠀⠀⠾⠿⠿⠟⠛⠛⠛⠛⠛⣛⣛⣛⣛⣛⡛⠛⠛⠛."⠀⠀⠀⠀⠀⠀
+print "⠀⠀⠀⠀⠀⠀⠰⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⢀⣤⣤⡀"⠀
+print "⠀⠀⠀⠀⢀⣴⡄⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⢀⣾⠋⡈⢿⡄"⠀
+print "⠀⠀⢠⣾⣿⣿⣿⣦⡀⠻⢿⣿⣿⣿⣿⣿⣿⠛⠛⠃⠀⠀⠀⣼⡇⠀⠁⢸⡇"⠀
+print "⠀⣠⣤⣤⣌⣉⠙⠻⢿⣦⣄⠙⠻⠿⣿⡿⠃⠰⣦⠀⠀⠀⠀⣿⡄⠀⠀⣼⠇"
+echo -e "⠀⣿⣿⣿⣿⣿⣿⣶⣤⣈⠛⢿⣶⣄⠀⠀⠀⠀⢸⠇⠀⠀⠀⠸⣧⣀⣰⠏"⠀
+echo -e "⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡈⠛⢷⠀⠀⠀⣾⠀⠀⠀⠀⠀⢸⡿⠁"⠀⠀⠀
+echo -e "⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣄⠀⠀⢸⣿⣿⣷⣦⠀⠀⢸⡇"⠀⠀⠀⠀
+echo -e "⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⠀⠘⠿⣿⠿⠋⠀⠀⣸⡇"⠀⠀⠀
+echo -e "⠀⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠀⠀⠀⠀⠀⠀⠀⠀⠛⠁"
 
+#!/bin/bash
+echo "detective@127.0.0.1: "
+
+# Operating system names are used here as a data source
+select number in Website Phonenumber Personal Image IP Exit.. 
+do
+
+case $number in
+# Choose
+"Website")
+python3 webinfo.py
+;;
+# Three case values are declared here for matching
+"Phonenumber")
+python3 phoneinfo.py
+;;
+"Personal")
+python3 personal.py
+;;
+"IP")
+python3 ip.py
+# Matching with invalid data
+*)
+echo "Invalid entry."
+break
+;;
+esac
+done
