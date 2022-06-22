320894e4edd6c17c781016ddef4a2b322f3a3a96
Mrx <90603785+official-hack4peace@users.noreply.github.com>
2022-06-22 13:24:31 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+grn="\e[92m"
+echo -e "$grn  ___  ___  ___  _ _  ___" 
+echo -e "     / __>| __>|_ _|| | || . \"
+echo -e "     \__ \| _>  | | | ' ||  _/"
+echo -e "     <___/|___> |_| `___'|_|"  
+
+apt install git
+apt install python3
+apt install php
+pip3 install request
+pip3 install pprint
+pip3 install socket
+pip3 install phonenumbers
