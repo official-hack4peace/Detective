204fac1a2d82f43eb38ab9c1c890eab207b16184
Mrx <90603785+official-hack4peace@users.noreply.github.com>
2022-06-22 14:09:05 +0530

    Update detective.sh

--- detective.sh
+++ detective.sh
@@ -18,10 +18,10 @@ echo -e "$blue⠀                  ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡈⠛⢷⠀
 echo -e "$blue⠀                  ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣄⠀⠀⢸⣿⣿⣷⣦⠀⠀⢸⡇ $rset"⠀⠀⠀⠀
 echo -e "$blue⠀                  ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⠀⠘⠿⣿⠿⠋⠀⠀⣸⡇ $rset"⠀⠀⠀
 echo -e "$blue⠀                  ⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠀⠀⠀⠀⠀⠀⠀⠀⠛⠁ $rset"
-echo -e "$blue                  Created by Mrx $rset"
-echo -e "$blue   GitHub: https://github.com/official-hack4peace $rset"
+echo -e "$blue                      Created by Mrx $rset"
+echo -e "$blue       GitHub: https://github.com/official-hack4peace $rset"
 echo -e "$blue   Instagram: https://instagram.com/anonymous_010101010101010 $rset"
-echo -e "$blue   YouTube: https://youtube.com/channel/UCHTT9mz7Fdtn3TdotMjynGg $rset"
+echo -e "$blue  YouTube: https://youtube.com/channel/UCHTT9mz7Fdtn3TdotMjynGg $rset"
 #!/bin/bash
 echo -e "$grn Welcome, Here you can find all information about your victim.. "
 
