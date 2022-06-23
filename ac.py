efa77fe5496f1a92e0b748686a8e7399e9a70e3f
Mrx <90603785+official-hack4peace@users.noreply.github.com>
2022-06-23 18:27:51 +0530

    Update ac.py

--- ac.py
+++ ac.py
@@ -1,13 +1,9 @@
-from bs4 import BeautifulSoup
-import requests
-
-url = input ("Enter url with username")
-request = requests.get(url)
-y = BeautifulSoup(request.text, "html.parser")
-meta = y.find("meta", property="og: description")
-y = meta.attrs['content'].split ("")
-data = {}
-data['Followers'] = y[0]
-data['Following'] = y[2]
-data['Posts'] = y[4]
-print (data)
+from instabot import Bot
+user = input("Enter username: ")
+pass = input("Enter password: ")
+bot = Bot()
+bot.login( username = (user), password = (pass))
+# Followers info
+followers = bot.get_user_followers(user)
+for follower in followers:
+     print(bot.get_user_info(follower))
