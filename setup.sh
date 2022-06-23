5e7bb43abba1268ae13d08ba22aa6de2493edae9
Mrx <90603785+official-hack4peace@users.noreply.github.com>
2022-06-23 13:10:37 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 grn="\e[92m"
-echo -e "$grn  ___  ___  ___  _ _  ___" 
-echo -e "     / __>| __>|_ _|| | || . \"
-echo -e "     \__ \| _>  | | | ' ||  _/"
-echo -e "     <___/|___> |_| `___'|_|"  
+rset="\e[0m"
+echo -e "$grn  ___  ___  ___  _ _  ___ $rset" 
+echo -e "     / __>| __>|_ _|| | || . \ $rset"
+echo -e "     \__ \| _>  | | | ' ||  _/ $rset"
+echo -e "     <___/|___> |_| `___'|_| $rset"  
 
 apt install git
 apt install python3
