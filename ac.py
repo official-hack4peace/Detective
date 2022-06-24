21c45beac87a4ecd09f06dd303646043030602a7
Mrx <90603785+official-hack4peace@users.noreply.github.com>
2022-06-24 14:11:05 +0530

    Update ac.py

--- ac.py
+++ ac.py
@@ -4,9 +4,9 @@ url = input("Enter url with username: ")
 vi = requests.get(url)
 print(vi.text)
 user = input("Enter username: ")
-pass = input("Enter password: ")
+code = input("Enter password: ")
 bot = Bot()
-bot.login( username = (user), password = (pass))
+bot.login( username = (user), password = (code))
 # Followers info
 followers = bot.get_user_followers(user)
 for follower in followers:
